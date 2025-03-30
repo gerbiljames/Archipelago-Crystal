8749daac7bf4d10357690e1241f1160fcdc8145f
James White <james_white_d@hotmail.com>
2025-03-30 22:15:57 +0100

    Remove logging statement

--- worlds/pokemon_crystal/rom.py
+++ worlds/pokemon_crystal/rom.py
@@ -227,7 +227,6 @@ def generate_output(world: "PokemonCrystalWorld", output_directory: str, patch: 
         address = data.rom_addresses["AP_TrainerParty_" + trainer_name]
         address += trainer_data.name_length + 1  # skip name and type
         for pokemon in trainer_data.pokemon:
-            print(f"writing level={pokemon.level} for pokemon {pokemon.pokemon} for trainer {trainer_name}")
             pokemon_data = [pokemon.level, data.pokemon[pokemon.pokemon].id]
             if pokemon.item is not None:
                 item_id = item_const_name_to_id(pokemon.item)
