5c4317578db5c73a33a6884d9eff6bee774085a8
Darvitz <vgm.shinshiba@gmail.com>
2025-08-13 01:43:02 -0400

    Update groups.py

--- worlds/pokemon_crystal/groups.py
+++ worlds/pokemon_crystal/groups.py
@@ -16,11 +16,11 @@ if 'RequiresSaffronGatehouses' in LOCATION_GROUPS_A:
 if 'VanillaClairOff' in LOCATION_GROUPS_A:
     del LOCATION_GROUPS_A['VanillaClairOff']        
 
-# # item_groups: Dict[str, Set[str]] = {}        
-# # for item in data.items.values():
-    # # for tag in item.tags:
-        # # if tag not in item_groups:
-            # # item_groups[tag] = set()
-        # # item_groups[tag].add(item.name)
+item_groups_a: Dict[str, Set[str]] = {}        
+for item in data.items.values():
+    for tag in item.tags:
+        if tag not in item_groups:
+            item_groups_a[tag] = set()
+        item_groups_a[tag].add(item.name)
 
 # The same can be done for item groups, if there's a need. Just have to create and import it under a different name than the one in items.py and then merge the two.
