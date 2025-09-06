c9f8a6b2a3b7383d234934984cbcd6b4e4ffe5ad
Darvitz <vgm.shinshiba@gmail.com>
2025-09-05 21:16:53 -0400

    Update wild.py

--- worlds/pokemon_crystal/wild.py
+++ worlds/pokemon_crystal/wild.py
@@ -82,7 +82,7 @@ def randomize_wild_pokemon(world: "PokemonCrystalWorld"):
             logical_pokemon_pool.extend(get_random_pokemon(world, blocklist=global_blocklist) for _ in
                                         range(required_logical_pokemon - len(logical_pokemon_pool)))
 
-        if world.options.goal == Goal.diploma:
+        if world.options.goal == Goal.option_diploma:
 
             if len(logical_pokemon_pool) < Dexcountsanity.value:
                 dexcount_diff = Dexcountsanity.value
