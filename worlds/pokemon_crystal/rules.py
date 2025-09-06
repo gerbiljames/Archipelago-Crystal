5b1cb026614916df38fc93a976241fcf65721639
Darvitz <vgm.shinshiba@gmail.com>
2025-09-06 16:52:43 -0400

    Update rules.py

--- worlds/pokemon_crystal/rules.py
+++ worlds/pokemon_crystal/rules.py
@@ -421,10 +421,10 @@ def set_rules(world: "PokemonCrystalWorld") -> None:
             "EVENT_BEAT_ELITE_FOUR", world.player)
 
     if world.options.goal == Goal.option_diploma:
-        event_loc = world.get_locations("EVENT_ENABLE_DIPLOMA_PRINTING", player)
+        event_loc = world.get_locations("EVENT_ENABLE_DIPLOMA_PRINTING", world.player)
 
         def diploma_rule(state):
-            return state.has(world.generated_dexcountsanity[-1], player)
+            return state.has(world.generated_dexcountsanity[-1], world.player)
 
         add_rule(event_loc, diploma_rule)
 
