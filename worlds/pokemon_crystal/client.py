41a3d70848e45abc2c1ab951f0c9d2daae00cf09
Darvitz <vgm.shinshiba@gmail.com>
2025-09-13 19:00:12 -0400

    Update client.py

--- worlds/pokemon_crystal/client.py
+++ worlds/pokemon_crystal/client.py
@@ -322,7 +322,11 @@ class PokemonCrystalClient(BizHawkClient):
                 ]
 
         elif ctx.slot_date["goal"] == Goal.option_defeat_team_rocket:
-            self.goal_flag = data.event_flags["EVENT_CLEARED_RADIO_TOWER"]
+            self.goal_flags = [
+                data.event_flags["EVENT_CLEARED_RADIO_TOWER"],
+                data.event_flags["EVENT_CLEARED_SLOWPOKE_WELL"],
+                data.event_flags["EVENT_CLEARED_ROCKET_HIDEOUT"],
+            ]
         else:
             self.goal_flag = data.event_flags["EVENT_BEAT_RED"]
 
