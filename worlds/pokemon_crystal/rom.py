582bb8a5bce881b69f15924dfd180fbac2cd31d9
Darvitz <vgm.shinshiba@gmail.com>
2025-09-28 10:54:11 -0400

    Update rom.py

--- worlds/pokemon_crystal/rom.py
+++ worlds/pokemon_crystal/rom.py
@@ -1020,6 +1020,10 @@ def generate_output(world: "PokemonCrystalWorld", output_directory: str, patch: 
         name_bytes = convert_to_ingame_text(world.options.trainer_name.value[:7])
         write_bytes(patch, name_bytes, data.rom_addresses["AP_Setting_DefaultTrainerName"])
 
+    if world.options.dexcountsanity:
+        dexcount = world.generated.dexcountsanity[-1]
+        write_bytes(patch, [1], data.rom_addresses["AP_Setting_DiplomaCount"] + 1)
+
     # Set slot auth
     ap_version_text = convert_to_ingame_text(data.manifest.world_version)[:19]
     ap_version_text.append(0x50)
