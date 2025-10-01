29b94b19bc40dab74db3c8973ca3ea1957dc3714
BlackSoulKnight <44965005+BlackSoulKnight@users.noreply.github.com>
2025-10-01 22:45:29 +0200

    Other things that still need a implementation

--- worlds/tevi/Web.py
+++ worlds/tevi/Web.py
@@ -1,5 +1,16 @@
 """Web UI related classes for Tevi"""
 from worlds.AutoWorld import WebWorld
+from BaseClasses import Tutorial
 
 class TeviWeb(WebWorld):
     """Web integration for Tevi"""
+    tutorials = [
+        Tutorial(
+            "Multiworld Setup Guide",
+            "A unfinished guide to setting up the Tevi randomizer connected to an Archipelago Multiworld.",
+            "English",
+            "setup_en.md",
+            "setup/en",
+            ["BlackSoulKnight"],
+        )
+    ]
