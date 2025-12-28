14a4bb0fe2b8b89225b465613d3bf3453960b0b8
Richard Snider <lighthousemaniac@gmail.com>
2025-12-28 02:06:16 -0700

    add bundle script to support ap 0.7 compliance
    (◔_◔)

--- worlds/crosscode/templates/common.template.py
+++ worlds/crosscode/templates/common.template.py
@@ -1,5 +1,8 @@
 {{generated_comment | indent("# ", True)}}
 
+from Utils import Version
+
 NAME: str = "CrossCode"
 BASE_ID: int = {{base_id}}
 DATA_VERSION: str = "{{data_version}}"
+APWORLD_VERSION: Version = Version({{version[0]}}, {{version[1]}}, {{version[2]}})
