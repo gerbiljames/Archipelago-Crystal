5789f93bb628bc58b20874bf11fa498209a1434f
James White <james_white_d@hotmail.com>
2026-02-05 01:22:16 +0000

    Add south kanto roadblocks to slot data

--- worlds/pokemon_crystal_prerelease/world.py
+++ worlds/pokemon_crystal_prerelease/world.py
@@ -606,6 +606,8 @@ class PokemonCrystalWorld(World):
             "enforce_breeding_methods_logic",
             "randomize_pokedex",
             "route_30_access",
+            "south_kanto_access",
+            "south_kanto_condition",
         )
         slot_data["apworld_version"] = self.apworld_version
         slot_data["tea_north"] = 1 if "North" in self.options.saffron_gatehouse_tea.value else 0
