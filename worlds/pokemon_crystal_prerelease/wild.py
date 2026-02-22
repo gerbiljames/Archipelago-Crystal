14a259a215afae1af25a32e5ffb66cadc5f55d13
Darvitz <vgm.shinshiba@gmail.com>
2026-02-22 00:03:20 -0500

    Allow true scholar goal to exclude Unown in wilds

--- worlds/pokemon_crystal_prerelease/wild.py
+++ worlds/pokemon_crystal_prerelease/wild.py
@@ -15,10 +15,8 @@ if TYPE_CHECKING:
 def randomize_wild_pokemon(world: "PokemonCrystalWorld"):
     if world.options.randomize_wilds and not world.is_universal_tracker:
 
-        if world.options.goal == Goal.option_unown_hunt:
+        if world.options.goal == Goal.option_unown_hunt or world.options.goal == Goal.option_true_scholar:
             exclude_unown = world.options.goal == Goal.option_unown_hunt
-        elif world.options.goal == Goal.option_true_scholar:
-            exclude_unown = world.options.goal == Goal.option_true_scholar
 
         world.generated_wooper = get_random_pokemon(world, exclude_unown=True)
 
