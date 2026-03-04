398f02881a6d1e4a92a2880b318cdbf683741050
Darvitz <vgm.shinshiba@gmail.com>
2026-03-04 02:10:07 -0500

    Modify level calculation for trainer Pokémon
    Adjust trainer Pokémon levels based on boost options.

--- worlds/pokemon_crystal_prerelease/trainers.py
+++ worlds/pokemon_crystal_prerelease/trainers.py
@@ -112,9 +112,12 @@ def boost_trainer_pokemon(world: "PokemonCrystalWorld"):
         for trainer_mon in trainer_data.pokemon:
             new_level = trainer_mon.level
             if world.options.boost_trainers == BoostTrainerPokemonLevels.option_percentage_boost:
-                new_level = int(trainer_mon.level * (world.options.trainer_level_boost / 100))
+                new_level = int(trainer_mon.level * (1 + world.options.trainer_level_boost / 100))
                 if new_level > 100: new_level = 100
                 elif new_level < 1: new_level = 1
+            elif world.options.boost_trainers == BoostTrainerPokemonLevels.option_percentage_decrease:
+                new_level = int(trainer_mon.level * ((100 - world.options.trainer_level_boost) / 100))
+                if new_level < 1: new_level = 1
             elif world.options.boost_trainers == BoostTrainerPokemonLevels.option_set_min_level:
                 if new_level < world.options.trainer_level_boost:
                     new_level = world.options.trainer_level_boost
