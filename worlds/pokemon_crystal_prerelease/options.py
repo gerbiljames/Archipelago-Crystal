5ef83c75bd588b513519ba6dffc9c9a33a9f9a3d
James White <james_white_d@hotmail.com>
2026-06-05 12:12:51 +0100

    Include kanto gyms in kinda_early_surf

--- worlds/pokemon_crystal_prerelease/options.py
+++ worlds/pokemon_crystal_prerelease/options.py
@@ -604,7 +604,8 @@ class KindaEarlySurf(Toggle):
     """
     Adds Surf as a logical requirement for: the Magnet Train, going east of Mahogany,
     fighting Jasmine, the Rocket takeover of the Radio Tower, entering Tin Tower,
-    and waking Snorlax. Forced off if Randomize Starting Town or Johto Only is enabled.
+    waking Snorlax, and entering the Kanto Gyms.
+    Forced off if Randomize Starting Town or Johto Only is enabled.
     """
     display_name = "Kinda Early Surf"
     visibility = Visibility.none
